e8ad73779a2b9c9ac044e246272b0d6a6aa9e639
shankar <shankarvpatil1599@gmail.com>
2021-06-02 18:50:02 +0530

    Compute4

--- Arithmatic_sorting.sh
+++ Arithmatic_sorting.sh
@@ -1,6 +1,6 @@
 echo "Enter space seperated a, b and c"
 read a b c
-echo "Entered values of a= $a, b= $b and c+ $c"
+echo "Entered values of a= $a, b= $b and c= $c"
 
-f1=$(($c+$a/$b))
-echo "$c+$a/$b= $f1"
+f1=$(($a%$b+$c))
+echo "$a%$b+$c= $f1"
