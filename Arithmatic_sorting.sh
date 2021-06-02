047f31a0acd821e565848e940090cf9182386918
shankar <shankarvpatil1599@gmail.com>
2021-06-02 18:56:16 +0530

    Add all results in one Dictionary

--- Arithmatic_sorting.sh
+++ Arithmatic_sorting.sh
@@ -1,6 +1,8 @@
 echo "Enter space seperated a, b and c"
 read a b c
-echo "Entered values of a= $a, b= $b and c= $c"
-
-f1=$(($a%$b+$c))
-echo "$a%$b+$c= $f1"
+declare -A res
+res['a+b*c']=$(($a+$b*$c))
+res['c+a/b']=$(($c+$a/$b))
+res['a*b+c']=$(($a*$b+$c))
+res['a%b+c']=$(($a%$b+$c))
+echo "Results of computation =${res[*]}"
